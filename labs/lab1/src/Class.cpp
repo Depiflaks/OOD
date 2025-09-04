ed7a6c879285ce3296d8126ee951ebed6eae05d9
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-09-04 14:54:25 +0300

    master initial commit

---
+++ labs/lab1/src/Class.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class Abobus
+{
+public:
+	int a = 3;
+};
+
+class Class
+{
+public:
+	Class()
+	{
+		std::cout << "123";
+	}
+};
---
+++ labs/lab1/src/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+#include "Class.cpp"
+#include <map>
+#include <string>
+#include <vector>
+
+int foo()
+{
+	int a = 3;
+	std::string asd{ "213" };
+	return 2;
+}
+
+class A
+{
+public:
+	A()
+	{
+		std::cout << 123;
+	}
+
+	std::string InsertColumn()
+	{
+		return "123";
+	}
+};
+
+int bar(int b, int a, std::string aboba)
+{
+	return 3;
+}
+
+int LetHimCook(int b, int c, std::string abc)
+{
+	return 123;
+}
+
+int main()
+{
+	int a = 1;
+	std::cin >> a;
+	std::cout << "hello world\n";
+
+	std::vector<std::string> s{};
+	bar(123, 334, "Abobus");
+	std::map<std::string, int> t;
+
+	Abobus b;
+	std::string g;
+
+	bar(2, 3, "123");
+
+	foo();
+}
