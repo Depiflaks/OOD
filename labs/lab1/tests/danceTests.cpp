ab966fb8bfb976a70070e402e39e1d92dc6838b0
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-09-10 17:08:09 +0300

    lab1 правки от Андрея Дудина

--- labs/lab1/tests/danceTests.cpp
+++ labs/lab1/tests/danceTests.cpp
@@ -58,5 +58,15 @@ TEST(DuckDanceTest, TestDanceBehaviorWhenChangeDanceBehavior)
 	duck.SetDanceBehavior(std::move(danceBehavior2));
 	duck.Dance();
 }
-// TODO: что после std::move
 
+TEST(DuckDanceTest, TestWhatHappendWithMovedOutStrategy)
+{
+	std::unique_ptr<MockDanceBehavior1> danceBehavior1 = std::make_unique<MockDanceBehavior1>();
+
+	std::unique_ptr<MockDanceBehavior2> danceBehavior2 = std::make_unique<MockDanceBehavior2>();
+
+	TestDuck duck(std::move(danceBehavior1));
+	duck.SetDanceBehavior(std::move(danceBehavior2));
+
+	EXPECT_FALSE(danceBehavior1);
+}
