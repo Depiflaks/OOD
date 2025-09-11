d77a1a81c79af4ce7d6ed91fc11f0f15206a904d
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-09-11 14:39:24 +0300

    lab1 класс для работы с минимальными объектами

---
+++ labs/lab1/shapes/src/Shapes/Geometry.h
@@ -0,0 +1,46 @@
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
+struct Position
+{
+	double x;
+	double y;
+
+	Position(double x = 0.0, double y = 0.0)
+		: x(x)
+		, y(y)
+	{
+	}
+};
+
+struct Size
+{
+	double w;
+	double h;
+
+	Size(double w = 0.0, double h = 0.0)
+		: w(w)
+		, h(h)
+	{
+	}
+};
+
+struct Bounds
+{
+	Position position;
+	Size size;
+
+	Bounds(Position pos = Position(), Size size = Size())
+		: position(pos)
+		, size(size)
+	{
+	}
+
+	Bounds(double x, double y, double w, double h)
+		: position(x, y)
+		, size(w, h)
+	{
+	}
+};
+
+#endif /* GEOMETRY_H */
