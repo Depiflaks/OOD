1a2f1b96eb0854a619511ee2ec073e3f675cf946
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-09-11 15:01:39 +0300

    lab1 класс Color

---
+++ labs/lab1/shapes/src/GFX/Color.h
@@ -0,0 +1,31 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include <string>
+#include <cstdint>
+#include <cmath>
+
+struct Color {
+    Color(const std::string& hexColor) {
+        if (hexColor.size() == 9 && hexColor[0] == '#') {
+            r = std::stoi(hexColor.substr(1, 2), nullptr, 16);
+            g = std::stoi(hexColor.substr(3, 2), nullptr, 16);
+            b = std::stoi(hexColor.substr(5, 2), nullptr, 16);
+            a = std::stoi(hexColor.substr(7, 2), nullptr, 16);
+        }
+    }
+
+    std::string ToHexString() const {
+        char buffer[10];
+        snprintf(buffer, sizeof(buffer), "#%02x%02x%02x%02x", r, g, b, a);
+        return std::string(buffer);
+    }
+
+    uint8_t r = 0;
+    uint8_t g = 0;
+    uint8_t b = 0;
+    uint8_t a = 255;
+};
+
+#endif /* COLOR_H */
+
