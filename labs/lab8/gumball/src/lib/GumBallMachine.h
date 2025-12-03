e47d5c820eb63871daad82c7e0b8e6d925bdb15e
smmm <ssmirnov130405@gmail.com>
2025-12-03 05:44:54 +0300

    lab8 кувырок кувырок сам себе отсосал

--- labs/lab8/gumball/src/lib/GumBallMachine.h
+++ labs/lab8/gumball/src/lib/GumBallMachine.h
@@ -2,6 +2,7 @@
 #include <format>
 #include <iostream>
 
+#include "State.h"
 
 struct IGumballMachine
 {
@@ -16,41 +17,187 @@ struct IGumballMachine
 	virtual ~IGumballMachine() = default;
 };
 
-
-class GumballMachine : private IGumballMachine
+class SoldState final : public IState
 {
 public:
-	GumballMachine(unsigned numBalls)
-		: m_soldState(*this)
+	explicit SoldState(IGumballMachine& gumballMachine)
+		: m_gumballMachine(gumballMachine)
+	{
+	}
+	void InsertQuarter() override
+	{
+		std::cout << "Please wait, we're already giving you a gumball\n";
+	}
+	void EjectQuarter() override
+	{
+		std::cout << "Sorry you already turned the crank\n";
+	}
+	void TurnCrank() override
+	{
+		std::cout << "Turning twice doesn't get you another gumball\n";
+	}
+	void Dispense() override
+	{
+		m_gumballMachine.ReleaseBall();
+		if (m_gumballMachine.GetBallCount() == 0)
+		{
+			std::cout << "Oops, out of gumballs\n";
+			m_gumballMachine.SetSoldOutState();
+		}
+		else
+		{
+			m_gumballMachine.SetNoQuarterState();
+		}
+	}
+	std::string ToString() const override
+	{
+		return "delivering a gumball";
+	}
+
+private:
+	IGumballMachine& m_gumballMachine;
+};
+
+class SoldOutState final : public IState
+{
+public:
+	explicit SoldOutState(IGumballMachine& gumballMachine)
+		: m_gumballMachine(gumballMachine)
+	{
+	}
+
+	void InsertQuarter() override
+	{
+		std::cout << "You can't insert a quarter, the machine is sold out\n";
+	}
+	void EjectQuarter() override
+	{
+		std::cout << "You can't eject, you haven't inserted a quarter yet\n";
+	}
+	void TurnCrank() override
+	{
+		std::cout << "You turned but there's no gumballs\n";
+	}
+	void Dispense() override
+	{
+		std::cout << "No gumball dispensed\n";
+	}
+	std::string ToString() const override
+	{
+		return "sold out";
+	}
+
+private:
+	IGumballMachine& m_gumballMachine;
+};
+
+class HasQuarterState final : public IState
+{
+public:
+	explicit HasQuarterState(IGumballMachine& gumballMachine)
+		: m_gumballMachine(gumballMachine)
+	{
+	}
+
+	void InsertQuarter() override
+	{
+		std::cout << "You can't insert another quarter\n";
+	}
+	void EjectQuarter() override
+	{
+		std::cout << "Quarter returned\n";
+		m_gumballMachine.SetNoQuarterState();
+	}
+	void TurnCrank() override
+	{
+		std::cout << "You turned...\n";
+		m_gumballMachine.SetSoldState();
+	}
+	void Dispense() override
+	{
+		std::cout << "No gumball dispensed\n";
+	}
+	std::string ToString() const override
+	{
+		return "waiting for turn of crank";
+	}
+
+private:
+	IGumballMachine& m_gumballMachine;
+};
+
+class NoQuarterState final : public IState
+{
+public:
+	explicit NoQuarterState(IGumballMachine& gumballMachine)
+		: m_gumballMachine(gumballMachine)
+	{
+	}
+
+	void InsertQuarter() override
+	{
+		std::cout << "You inserted a quarter\n";
+		m_gumballMachine.SetHasQuarterState();
+	}
+	void EjectQuarter() override
+	{
+		std::cout << "You haven't inserted a quarter\n";
+	}
+	void TurnCrank() override
+	{
+		std::cout << "You turned but there's no quarter\n";
+	}
+	void Dispense() override
+	{
+		std::cout << "You need to pay first\n";
+	}
+	std::string ToString() const override
+	{
+		return "waiting for quarter";
+	}
+
+private:
+	IGumballMachine& m_gumballMachine;
+};
+
+class GumballMachine final : IGumballMachine
+{
+public:
+	explicit GumballMachine(unsigned numBalls)
+		: m_count(numBalls)
+		, m_soldState(*this)
 		, m_soldOutState(*this)
 		, m_noQuarterState(*this)
 		, m_hasQuarterState(*this)
 		, m_state(&m_soldOutState)
-		, m_count(numBalls)
 	{
 		if (m_count > 0)
 		{
 			m_state = &m_noQuarterState;
 		}
 	}
+
 	void EjectQuarter()
 	{
 		m_state->EjectQuarter();
 	}
+
 	void InsertQuarter()
 	{
 		m_state->InsertQuarter();
 	}
+
 	void TurnCrank()
 	{
 		m_state->TurnCrank();
 		m_state->Dispense();
 	}
+
 	std::string ToString() const
 	{
 		return std::format(R"(
 Mighty Gumball, Inc.
-C++-enabled Standing Gumball Model #2016
+C++-enabled Standing Gumball Model #2025
 Inventory: {} gumball{}
 Machine is {}
 )",
@@ -62,7 +209,8 @@ private:
 	{
 		return m_count;
 	}
-	virtual void ReleaseBall() override
+
+	void ReleaseBall() override
 	{
 		if (m_count != 0)
 		{
@@ -70,24 +218,27 @@ private:
 			--m_count;
 		}
 	}
+
 	void SetSoldOutState() override
 	{
 		m_state = &m_soldOutState;
 	}
+
 	void SetNoQuarterState() override
 	{
 		m_state = &m_noQuarterState;
 	}
+
 	void SetSoldState() override
 	{
 		m_state = &m_soldState;
 	}
+
 	void SetHasQuarterState() override
 	{
 		m_state = &m_hasQuarterState;
 	}
 
-private:
 	unsigned m_count = 0;
 	SoldState m_soldState;
 	SoldOutState m_soldOutState;
