e0952c5161a140f0231f0c2fecd5adcdca4ba6d1
smmm <ssmirnov130405@gmail.com>
2025-12-18 12:18:33 +0300

    lab10 тестовое окошко

--- labs/lab10/mvc/src/main.cpp
+++ labs/lab10/mvc/src/main.cpp
@@ -1,34 +1,75 @@
-//
-// Created by smmm on 12/18/25.
-//
-
 #include <QApplication>
 #include <QWidget>
-#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
 #include <QVBoxLayout>
+#include <QLabel>
 
-int main(int argc, char *argv[]) {
-	// 1. Создаем объект приложения
-	QApplication app(argc, argv);
+class Controller : public QObject
+{
+    Q_OBJECT
 
-	// 2. Создаем главное окно (виджет)
-	QWidget window;
-	window.setWindowTitle("Тестовое окно Qt 6");
-	window.resize(400, 300);
+public:
+    Controller(QLineEdit *inputField, QLabel *label, QObject *parent = nullptr)
+        : QObject(parent), m_inputField(inputField), m_label(label)
+    {}
 
-	// 3. Создаем контент (надпись)
-	QVBoxLayout *layout = new QVBoxLayout(&window);
-	QLabel *label = new QLabel("Привет! Qt и CMake работают успешно.", &window);
-	label->setAlignment(Qt::AlignCenter);
+public slots:
+    void onButtonClicked()
+    {
+        QString text = m_inputField->text();
+        m_label->setText("You entered: " + text);
+    }
 
-	// Добавляем стиль для красоты
-	label->setStyleSheet("font-size: 18px; color: #2c3e50; font-weight: bold;");
+private:
+    QLineEdit *m_inputField;
+    QLabel *m_label;
+};
 
-	layout->addWidget(label);
+class MainWindow : public QWidget
+{
+    Q_OBJECT
 
-	// 4. Показываем окно
-	window.show();
+public:
+    MainWindow(QWidget *parent = nullptr) : QWidget(parent)
+    {
+        // Создание виджетов
+        m_inputField = new QLineEdit(this);
+        m_label = new QLabel("Enter something:", this);
+        m_button = new QPushButton("Submit", this);
 
-	// 5. Запускаем цикл обработки событий
-	return app.exec();
+        // Создание контроллера
+        m_controller = new Controller(m_inputField, m_label, this);
+
+        // Сигналы и слоты
+        connect(m_button, &QPushButton::clicked, m_controller, &Controller::onButtonClicked);
+
+        // Размещение виджетов на окне
+        QVBoxLayout *layout = new QVBoxLayout(this);
+        layout->addWidget(m_label);
+        layout->addWidget(m_inputField);
+        layout->addWidget(m_button);
+
+        setLayout(layout);
+        setWindowTitle("Qt Example");
+        resize(300, 150);
+    }
+
+private:
+    QLineEdit *m_inputField;
+    QLabel *m_label;
+    QPushButton *m_button;
+    Controller *m_controller;
+};
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    MainWindow window;
+    window.show();
+
+    return app.exec();
 }
+
+#include "main.moc"
