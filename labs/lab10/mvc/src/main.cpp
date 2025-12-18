8163975ef1050fc14f1973db7dc6202aaa870bb6
smmm <ssmirnov130405@gmail.com>
2025-12-19 02:13:08 +0300

    lab10 чуть чуть больше архитектуры...

--- labs/lab10/mvc/src/main.cpp
+++ labs/lab10/mvc/src/main.cpp
@@ -1,75 +1,65 @@
-#include <QApplication>
-#include <QWidget>
-#include <QLineEdit>
-#include <QPushButton>
-#include <QVBoxLayout>
-#include <QLabel>
+#include <boost/signals2.hpp>
+#include <iostream>
 
-class Controller : public QObject
+class Subject
 {
-    Q_OBJECT
-
 public:
-    Controller(QLineEdit *inputField, QLabel *label, QObject *parent = nullptr)
-        : QObject(parent), m_inputField(inputField), m_label(label)
-    {}
+	boost::signals2::signal<void()> stateChangedSignal;
+	boost::signals2::signal<void(int)> taskCompletedSignal;
 
-public slots:
-    void onButtonClicked()
-    {
-        QString text = m_inputField->text();
-        m_label->setText("You entered: " + text);
-    }
+	void changeState()
+	{
+		std::cout << "State has been changed!" << std::endl;
+		stateChangedSignal();
+	}
 
-private:
-    QLineEdit *m_inputField;
-    QLabel *m_label;
+	void completeTask(int taskId)
+	{
+		std::cout << "Task " << taskId << " completed!" << std::endl;
+		taskCompletedSignal(taskId);
+	}
 };
 
-class MainWindow : public QWidget
+class StateObserver
 {
-    Q_OBJECT
-
 public:
-    MainWindow(QWidget *parent = nullptr) : QWidget(parent)
-    {
-        // Создание виджетов
-        m_inputField = new QLineEdit(this);
-        m_label = new QLabel("Enter something:", this);
-        m_button = new QPushButton("Submit", this);
-
-        // Создание контроллера
-        m_controller = new Controller(m_inputField, m_label, this);
-
-        // Сигналы и слоты
-        connect(m_button, &QPushButton::clicked, m_controller, &Controller::onButtonClicked);
-
-        // Размещение виджетов на окне
-        QVBoxLayout *layout = new QVBoxLayout(this);
-        layout->addWidget(m_label);
-        layout->addWidget(m_inputField);
-        layout->addWidget(m_button);
-
-        setLayout(layout);
-        setWindowTitle("Qt Example");
-        resize(300, 150);
-    }
-
-private:
-    QLineEdit *m_inputField;
-    QLabel *m_label;
-    QPushButton *m_button;
-    Controller *m_controller;
+	void onStateChanged()
+	{
+		std::cout << "StateObserver: The state has changed!" << std::endl;
+	}
 };
 
-int main(int argc, char *argv[])
+class TaskObserver
 {
-    QApplication app(argc, argv);
+public:
+	void onTaskCompleted(int taskId)
+	{
+		std::cout << "TaskObserver: Task " << taskId << " is completed!"
+				  << std::endl;
+	}
+};
 
-    MainWindow window;
-    window.show();
+int main()
+{
+	Subject subject;
+	StateObserver stateObserver;
+	TaskObserver taskObserver;
 
-    return app.exec();
+	subject.stateChangedSignal.connect(
+		[&stateObserver]() { stateObserver.onStateChanged(); });
+
+	subject.taskCompletedSignal.connect(
+		[&taskObserver](int taskId) { taskObserver.onTaskCompleted(taskId); });
+
+	subject.changeState();
+	subject.completeTask(1);
+
+	TaskObserver anotherTaskObserver;
+	subject.taskCompletedSignal.connect([&anotherTaskObserver](int taskId) {
+		anotherTaskObserver.onTaskCompleted(taskId);
+	});
+
+	subject.completeTask(2);
+
+	return 0;
 }
-
-#include "main.moc"
