c7a1d88b775b66ea8df544d2dd8dae29c51979ad
smmm <ssmirnov130405@gmail.com>
2026-01-13 20:33:33 +0300

    lab10_gio что-то заработало

--- labs/lab10/mvvm/main.go
+++ labs/lab10/mvvm/main.go
@@ -17,7 +17,7 @@ import (
 func main() {
 	go func() {
 		w := new(app.Window)
-		w.Option(app.Title("Gio v0.9 Demo"), app.Size(unit.Dp(400), unit.Dp(400)))
+		w.Option(app.Title("Gio Click Demo"), app.Size(unit.Dp(400), unit.Dp(400)))
 
 		if err := run(w); err != nil {
 			log.Fatal(err)
@@ -30,32 +30,20 @@ func main() {
 func run(w *app.Window) error {
 	var ops op.Ops
 
-	// ДЛЯ ВЕРСИИ v0.9.0 ИСПОЛЬЗУЕМ range w.Events()
-	for event := range w.Events() {
-		switch event := event.(type) {
-
+	for {
+		switch event := w.Event().(type) {
 		case app.DestroyEvent:
 			return event.Err
-
 		case app.FrameEvent:
-			// 1. Создаем контекст
 			gtx := app.NewContext(&ops, event)
-
-			// 2. Рисуем фон
 			paint.Fill(gtx.Ops, color.NRGBA{R: 30, G: 30, B: 35, A: 255})
-
-			// 3. Рисуем круг
 			drawCircle(gtx)
-
-			// 4. Завершаем кадр
 			event.Frame(gtx.Ops)
 		}
 	}
-	return nil
 }
 
 func drawCircle(gtx layout.Context) {
-	// Рисуем круг по центру
 	defer clip.Ellipse{
 		Min: image.Pt(100, 100),
 		Max: image.Pt(200, 200),
